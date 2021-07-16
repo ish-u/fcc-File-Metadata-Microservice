456b4d37564b07e3cf2253d42dfc4b1bf3e0db57
Anmol Gupta <anmolgupta520@gmail.com>
2021-07-16 15:20:34 +0530

    added solution

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # [File Metadata Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice)
+
+### Taken from [FCC](https://github.com/freeCodeCamp/boilerplate-project-filemetadata/)
+
+### [REPLit](https://replit.com/@ishu2/boilerplate-project-filemetadata)
--- server.js
+++ server.js
@@ -1,20 +1,30 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+var multer = require("multer");
+var upload = multer({ dest: "uploads/" });
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+// file analyse route
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const file = req.file;
+  const file_meta_data = {
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size,
+  };
+  res.send(file_meta_data);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
